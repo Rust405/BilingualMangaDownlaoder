072c261eceb0ffb54eaadc4e7ea001eb09b2525e
Russell Len <russelllen405@gmail.com>
2022-07-25 15:40:07 +0800

    initial commit

---
+++ readme.txt
@@ -0,0 +1,13 @@
+1. Requires fpdf, execute "py -m pip install fpdf" before running if not installed
+2. There's no error handling at the moment
+
+To use, 
+1. Enter URL of last image in volume, example : https://ipfs.io/ipfs/bafybeieiavplh2hz6kssubijy2fevuduh7lejhfdrlwlrl5ojfyyrnvigi/87274eeac32f16e2acd382f20b39a0eb/P00162.jpg
+
+2. Wait for download to complete
+
+3. Enter file name
+
+4. Done, repeat for each volume.
+
+If it fails just run the script again, it will clear the download folder and restart.
---
+++ script.py
@@ -0,0 +1,69 @@
+import urllib.request
+import os
+import glob
+import time
+from fpdf import FPDF
+
+
+def makeDirectories():
+    if not os.path.exists('download'):
+        os.makedirs('download')
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+
+def downloadJPG():
+    lastImgURL = input("> Image URL of last image: ")
+    lastPhotoNumber = lastImgURL[-9:-4]
+    numberOfPhotos = int(lastPhotoNumber)
+    imgURL = lastImgURL[:-10]
+
+    for i in range(1, int(numberOfPhotos) + 1):
+
+        file = 'P' + str(i).zfill(5) + '.jpg'
+        downloadURL = imgURL + file
+
+        start = time.time()
+        urllib.request.urlretrieve(downloadURL, "download/" + file)
+        end = time.time()
+
+        os.system("cls")
+        print('Downloading ' + file + '...')
+        progress = i/int(numberOfPhotos)*100
+        print('Download Progress: ' + "{:6.2f}".format(progress) + '%')
+
+        diff = end - start
+        estimate = int(diff) * int(numberOfPhotos) - i
+        if estimate < 0:
+            estimate = 0
+        print('Estimated time remaining: ' + str(estimate) + ' seconds')
+
+    print("Done!\n")
+
+
+def convertToPDF():
+    title = input("> Title of output file: ")
+
+    print("\nConverting to pdf...")
+    images = glob.glob('download/*.jpg')
+
+    pdf = FPDF()
+    for image in images:
+        pdf.add_page()
+        pdf.image(image, 0, 0, 210, 297)
+    pdf.output("output/" + title + ".pdf", "F")
+    print("Done!\n")
+
+
+def clearDownloadFolder():
+    trash = glob.glob('download/*')
+    for t in trash:
+        os.remove(t)
+
+
+os.system("cls")
+makeDirectories()
+clearDownloadFolder()
+downloadJPG()
+convertToPDF()
+clearDownloadFolder()
